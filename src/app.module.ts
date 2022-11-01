5907a6d6894738c3237807098a595601ec20f696
Max <maxsoltys47@gmail.com>
2022-11-01 21:03:53 +0200

    Created .local.env, updated AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,25 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodoModule } from './modules/todo/todo.module';
 import { UserModule } from './modules/user/user.module';
+import {ConfigModule, ConfigService} from "@nestjs/config";
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'ms26052003',
-      database: 'todoApiDB',
-      entities: ['dist/**/*.entity{.ts,.js}'],
-      synchronize: true,
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: ['.local.env'] }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        synchronize: configService.get<boolean>('DB_SYNC'),
+        logging: configService.get<boolean>('DB_LOGGING'),
+        database: configService.get('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      }),
     }),
     TodoModule,
     UserModule,
